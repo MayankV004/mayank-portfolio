4ce330729757e57ca182367210aeed274e4ee277
mayank verma <mayank.msverma@gmail.com>
2026-05-17 06:45:30 +0530

    resume link updated

--- src/components/ui/ResumeButton.tsx
+++ src/components/ui/ResumeButton.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { ArrowRight, Download } from "lucide-react";
 
 const RESUME_URL =
-  "https://drive.google.com/file/d/1ojssFegHf7Jhw2nk7p6DnLXENS3sO52A/view?usp=sharing";
+  "https://drive.google.com/file/d/1i8cGaR9k2wTZlzkZKeV0jprrG9-0bhZg/view?usp=sharing";
 
 export function ResumeButton() {
   const [isHovered, setIsHovered] = useState(false);
